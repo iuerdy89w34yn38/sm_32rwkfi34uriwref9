fc257eead2a238eb23babe4d1f23ef464eae617d
iuerdy89w34yn38 <iuerdy89w34yn38_c7y83tc9wy89w7yty38_234234@wilcode.com>
2021-01-15 08:14:49 +0500

    walletupdate

--- wallet.php
+++ wallet.php
@@ -15,7 +15,7 @@ error_reporting(false);
 	<!-- Document Title
 	============================================= -->
   
-  <title>Wallet Login - <?php echo $sitename ?> </title>
+  <title>Wallet - <?php echo $sitename ?> </title>
 
 	<?php $link="blank" ?>
 
@@ -31,155 +31,164 @@ error_reporting(false);
 
 		<!-- Header
 		============================================= -->
-		<?php //	include 'include/header.php'; ?>
+		<?php	include 'include/header.php'; ?>
 
 	
 
- <div class="wtop">
-  <div class="row" style="width: 99%">
-    <div class="col-md-2" style="padding: 20px; padding-left: 40px">
-      <a href="#">
-      <img src="images/wallet/s.png" style="width: 100px">
-        
-      </a>
-    </div>
-    <div class="col-md-8">
-    </div>
-    <div class="col-md-2 " style="text-align: right;padding: 20px">
-      <a href="#">
-      <img src="images/wallet/w.png" style="width: 120px">
-        
-      </a>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+ -->
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
 
-      
+
+
+
+      <div class="item active" style="background:url(images/wallet/w2.jpg); ">
+
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-6">
+            <div style="padding-top: 90px" class="animated fadeInLeft">
+             <br>  
+          <h2 style="color: #000">Swift E-Wallet</h2>
+          <br>
+
+          <p style="color: #000">
+            Insightful pay quickly with just an email address and password. Enable card, bank and local payment methods and connect with millions of existing Skrill wallet holders globally..
+
+
+            <br>
+            <br>
+            1-Tap allows customers to make quick, repeat payments with a single touch, without having to re-enter their log-in details or payment information. They can pay on the go or pay while they play, all within seconds from any device. Quick and easy to enable, add Skrill 1-Tap to your checkout and benefit from fast, effective and frictionless payments.
+            <br>
+            <br>
+          </p>
+        </div>
+
+
+            
+          </div>
+          
+
+      </div>
     </div>
 
+
+      <div class="item" style="background:url(images/wallet/w3.jpg); ">
+
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-6">
+            <div style="padding-top: 90px" class="animated fadeInLeft">
+             <br>  
+          <h2 style="color: #000">Interbank Transfers</h2>
+          <br>
+
+          <p style="color: #000">
+            Digital Wallet will look on your website and view the online customer journey to the point of purchase and connect with millions of existing Skrill wallet holders globally..
+
+
+            <br>
+            <br>
+            Grow and connect
+With millions of existing Skrill Wallet holders globally. Quick and easy to enable, add Skrill 1-Tap to your checkout and benefit from fast, effective and frictionless payments.Improve conversion rates
+Customers pay quickly with just an email address and password.Chargeback protection
+Payments are indemnified
+            <br>
+            <br>
+          </p>
+        </div>
+
+
+            
+          </div>
+          
+        </div>
+      </div>
+
+
+
+      <div class="item" style="background:url(images/wallet/w1.jpg); ">
+
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-6">
+            <div style="padding-top: 90px" class="animated fadeInLeft">
+             <br>  
+          <h2 style="color: #000; text-transform: capitalize;"> connect Wallet holders globally</h2>
+          <br>
+
+          <p style="color: #000">
+            With millions of existing Skrill Wallet holders globally will look on your website and view the online customer journey to the point of purchase and connect with millions of existing Skrill wallet holders globally..
+
+
+            <br>
+            <br>
+            Chargeback protection
+Payments are indemnified. Grow and connect
+With millions of existing Skrill Wallet holders globally. Quick and easy to enable, add Skrill 1-Tap to your checkout and benefit from fast, effective and frictionless payments.Improve conversion rates
+Customers pay quickly with just an email address and password.Chargeback protection
+Payments are indemnified
+            <br>
+            <br>
+          </p>
+        </div>
+
+
+            
+          </div>
+          
+        </div>
+      </div>
+
+
+
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" style="    background: none; left: -50px" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" style="    background: none; right: -50px" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
-		<!-- Content
-		============================================= -->
-		<section id="content" style="background: linear-gradient(132deg, rgba(30,239,255,1) 0%, rgba(0,147,177,1) 100%); padding: 120px">
-
-			<div class="content-wrap">
-
-				<div class="container clearfix">
 
 
-<div class="row ">
-
-<div class="col-md-4">
-</div>
-<div class="col-md-4">
-
-	<style type="text/css">
-   input{
-    text-align: center;
-   } 
+  <style type="text/css">
+    
+    .item{
+      min-height: 490px;
+    background-size: contain !important;
+    background-repeat: no-repeat !important;
+    background-position: right !important;
+}
   </style>
 
 
-						<div class="tab-container">
+            
+           </div>
 
-							<div class="tab-content clearfix" id="tab-login">
-								<div class="card nobottommargin">
-									<div class="card-body" style="padding: 40px;">
-										<form action="" method="post">
-								
-											<h1 class="text-center">
-       <h2 class="text-center">   <a href="#" style="text-transform: uppercase;">Log in to your Wallet</a> </h2>
-       <br>
-        </h1>
-        <form >
-          <div class="form-group">
-            <input type="email" class="form-control" name="username" placeholder="‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏S-ID" type="text">
-          </div>
-          <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password">
-          </div>
+           </div>
 
-          <div class="form-group text-center">
+      
 
-            <input type="submit" name="login" class="form-control btn bgcolor" value="Login">
-          </div>
-          
-        </form>
-          <div class="form-group text-center">
-            <a href="#">Forgot Password </a>
-          </div>
-
-        <?php
-                       
-           if (isset($_POST['login'])) {
-                       if (empty($_POST['username']) || empty($_POST['password'])) {
-                       echo "Email or Password is empty";
-                       }
-                       else
-                       {
-                       $username=$_POST['username'];
-                       $password=$_POST['password'];
-                       $username3 = stripslashes($username);
-                       $username2 = str_replace("<", "", $username3);
-                       $username1 = str_replace(">", "", $username2);
-                       $username = str_replace("'", "", $username1);
-                       $password3 = stripslashes($password);
-                       $password2 = str_replace("<", "", $password3);
-                       $password1 = str_replace(">", "", $password2);
-                       $password = str_replace("'", "", $password1);
+        </div>
+      </div>
 
 
-
-                       $query = mysqli_query($con,"SELECT * from users where password='$password' AND email='$username' ")or die(mysqli_error($con));
-                       $rows = mysqli_num_rows($query);
-                       if ($rows == 1) {
-
-
-
-      $rowsx =mysqli_query($con,"SELECT * from users where password='$password' AND email='$username' LIMIT 1 " ) or die(mysqli_error($con));
-      while($rowx=mysqli_fetch_array($rowsx)){
-        $actid = $rowx['id'];
-        $block = $rowx['block'];
-        $blockres = $rowx['blockres'];
-    }
-
-                      if($block==0){
-                       $_SESSION['actid']=$actid; // Initializing Session
-                       header("location:profile.php"); // Redirecting To Other Page
-                     }else{
-                      echo "Account Blocked: ".$blockres;
-                     }
-                       } else {
-                        echo "Email or Password is Invalid";
-
-                       }
-                       }
-                       }
-
-                       ?>
-
-
-
-									</div>
-
-
-								</div>
-
-							</div>
-
-
-
-
-						</div>
-
-					</div>
-             <div class="col-md-1">
-              <br><br>
-          
-
-				</div>
-
-			</div>
-
-		</section><!-- #content end -->
-
+    </div>
+  </div>
 
 		<!-- Footer
 		============================================= -->
